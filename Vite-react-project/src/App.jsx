f0c2d005e2ae170fac63a2b50e9fe3815295f55d
Adhiviraj <paladhiviraj@gmail.com>
2026-07-17 16:49:24 +0530

    vite - react project

---
+++ Vite-react-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai";
+
+function App() {
+
+  return (
+    <>
+      <Chai/>
+      <h1>You can export only on element or tag</h1>
+      <h1>But inside one element you can export inside many element</h1>
+    </>
+  )
+}
+
+export default App
